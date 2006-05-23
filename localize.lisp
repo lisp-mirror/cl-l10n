4890d1d260ee7bd04995b74f650115be6adf4f0d
attila.lendvai <attila.lendvai@gmail.com>
2006-05-23 15:46:53 +0000

    Fix lookup-resource mechanism, implicit progn, etc
    darcs-hash:60a5a555b8ea5c6d2a4a7a1f447a770e97285e7f

--- localize.lisp
+++ localize.lisp
@@ -14,36 +14,43 @@
 (define-condition resource-missing (warning)
   ((name :accessor name-of :initarg :name)))
 
-(defun add-resource (locale name args &rest body)
+(defun add-resource (locale name args body)
   ;; store in resouce map
   (setf (gethash (resource-key locale name) *resources*)
-        (if args
-            (eval `(lambda ,args ,@body))
-            (first body)))
+        (if (and (= (length body) 1)
+                 (stringp (first body)))
+            (first body)
+            (eval `(lambda ,args ,@body))))
   ;; make a function 
   (setf (symbol-function name) (eval `(lambda (&rest args) (lookup-resource-with-fallback ',name args))))
   name)
 
-(defun lookup-resource-with-fallback (name args &rest rest &key &allow-other-keys)
-  (loop for locale in (or *fallback-locales* (list *locale*)) do
-        (let ((result (apply 'lookup-resource locale name args rest)))
-          (when result
-            (return result)))))
-
-(defun lookup-resource (locale name args &key (warn-if-missing t) (fallback-to-name t))
+(defun %lookup-resource (locale name args)
   (let ((resource (gethash (resource-key locale name) *resources*)))
     ;; dispatch on resource type
     (cond ((functionp resource)
            (apply resource args))
           ;; literal
           ((not (null resource))
-           resource)
-          ;; not found
-          (t
-           (if warn-if-missing
-             (signal 'resource-missing :name name))
-           (if fallback-to-name
-               (string-downcase (string name)))))))
+           resource))))
+
+(defun lookup-resource-with-fallback (name args &key (warn-if-missing t) (fallback-to-name t))
+  (loop for locale in (or *fallback-locales* (list *locale*)) do
+        (let ((result (funcall '%lookup-resource locale name args)))
+          (when result
+            (return-from lookup-resource-with-fallback result))))
+  (resource-not-found name warn-if-missing fallback-to-name))
+
+(defun lookup-resource (locale name args &key (warn-if-missing t) (fallback-to-name t))
+  (aif (%lookup-resource locale name args)
+       it
+       (resource-not-found name warn-if-missing fallback-to-name)))
+
+(defun resource-not-found (name warn-if-missing fallback-to-name)
+  (if warn-if-missing
+      (signal 'resource-missing :name name))
+  (if fallback-to-name
+      (string-downcase (string name))))
 
 (defun locale-name-for-symbol (symbol)
   (let ((parts (split-sequence:split-sequence #\- (symbol-name symbol))))
@@ -60,10 +67,10 @@
                (loop for resource in resources
                      if (= 2 (length resource))
                      collect `(add-resource ,',locale-name
-                               ',(first resource) ,',() ,(second resource))
+                               ',(first resource) ,',() ',(cdr resource))
                      else
                      collect `(add-resource ,',locale-name
-                               ',(first resource) ',(second resource) ',(third resource)))))
+                               ',(first resource) ',(second resource) ',(cddr resource)))))
       (export ',name))))
 
 (defmacro with-locale (name &body body)
