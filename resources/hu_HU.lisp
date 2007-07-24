f96c1b441db3d04cbac393e9db89a63371742dc7
attila.lendvai <attila.lendvai@gmail.com>
2007-07-24 21:57:21 +0000

    Get rid of a warning in hungarian resources
    darcs-hash:9123c3accfda87512113a49e380bde2ea84a8623

--- resources/hu_HU.lisp
+++ resources/hu_HU.lisp
@@ -3,6 +3,7 @@
   (yes "igen")
   (no "nem")
   (indefinit-article-for (str)
+                         (declare (ignore str))
                          "egy")
   (plural-of (str)
              (hungarian-plural-of str)))
