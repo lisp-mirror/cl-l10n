4b9fe04538c4786b1a2d411a1f85fa3ce52ea946
attila.lendvai <attila.lendvai@gmail.com>
2011-07-27 13:57:27 +0000

    test/ more decimal formatting tests, some of them fail
    Ignore-this: 7505c1c41bd982d5a50c9f086e0fc5c2
    
    darcs-hash:0679582830e6e5a806d318308b2ebee27f2d2794

--- test/formatting.lisp
+++ test/formatting.lisp
@@ -6,14 +6,26 @@
 (defsuite* (test/cldr/formatting :in test/cldr))
 
 (deftest (test/cldr/formatting/bug/1) ()
-  (bind ((compiled-pattern (compile-number-pattern/decimal "#,##0.###")))
+  (flet ((to-string (number)
+           (format-number/decimal nil number :pattern "#,##0.###")))
+    (is (string= (to-string 3.9990) "3.999"))
+    (is (string= (to-string 3.9995) "3.999"))
+    (is (string= (to-string 3.9995d0) "3.999"))
+    (is (string= (to-string 3.9996) "4"))
+    (is (string= (to-string 3.9999) "4"))
+    (is (string= (to-string 3.99999999999d0) "4"))
+    (is (string= (to-string 4) "4"))))
+
+;; i'm not sure if the following two is strictly speaking a bug, but it's definitely annoying behaviour...
+(deftest (test/cldr/formatting/bug/2) ()
+  (with-expected-failures
+    (is (string= (format-number/decimal nil 3.9995d0 :pattern "#,##0.########################")
+                 "3.9995"))))
+
+(deftest (test/cldr/formatting/bug/3) ()
+  (bind ((number (/ 7999 2000)))
     (flet ((to-string (number)
-             (bind ((stream nil))
-               (with-normalized-stream-variable stream
-                 (funcall compiled-pattern stream number)))))
-      (is (string= (to-string 3.9990) "3.999"))
-      (is (string= (to-string 3.9995) "3.999"))
-      (is (string= (to-string 3.9996) "4"))
-      (is (string= (to-string 3.9999) "4"))
-      (is (string= (to-string 3.99999999999d0) "4"))
-      (is (string= (to-string 4) "4")))))
+             (format-number/decimal nil number :pattern "#,##0.###")))
+      (with-expected-failures
+        (is (string= (to-string number)
+                     (to-string (coerce number 'double-float))))))))
